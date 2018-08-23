ddfea6e1b3e9c2b53baa0f7b56fc41cbffbe65b9
Hirmuolio <hirmuolio@gmail.com>
2018-08-23 21:16:10 +0300

    Add files via upload

---
+++ contracts.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+
+import json
+import esi_calling
+import requests
+import datetime
+
+def fetch_contracts(region_id):
+	#10000044 = Solitude
+	print('fetching contracts')
+	
+	all_contracts = []
+	
+	#/v1/contracts/public/{par}/
+	response = esi_calling.call_esi(scope = '/v1/contracts/public/{par}/', url_parameter=region_id, job = 'get region contracts')
+	
+	all_contracts.extend(response.json())
+	total_pages = int(response.headers['X-Pages'])
+	print('total number of pages:'+str(total_pages))
+	
+	#Import rest of the pages
+	responses = []
+	for page in range(2, total_pages + 1):
+		print('\rimportin page: '+str(page)+'/'+str(total_pages), end="")
+		response = esi_calling.call_esi(scope = '/v1/contracts/public/{par}/', url_parameter=region_id, parameters = {'page': page}, job = 'get region contracts')
+		responses.append(response)
+	for response in responses:
+		data = response.json()
+		all_contracts.extend(data)
+	print('. Got {:,d} contracts.'.format(len(all_contracts)))
+	return all_contracts
+
+
+def import_orders(region_id):
+	#'10000044' Solitude
+	#10000002 = Jita
+	print('importin page 1')
+	all_orders = []
+	
+	response = esi_calling.call_esi(scope = '/v1/markets/{par}/orders/', url_parameter=region_id, job = 'get market orders')
+	
+	all_orders.extend(response.json())
+	total_pages = int(response.headers['X-Pages'])
+	print('total number of pages:'+str(total_pages))
+	
+	responses = []
+	for page in range(2, total_pages + 1):
+		print('\rimportin page: '+str(page)+'/'+str(total_pages), end="")
+		
+		response = esi_calling.call_esi(scope = '/v1/markets/{par}/orders/', url_parameter=region_id, parameters = {'page': page}, job = 'get market orders')
+		
+		responses.append(response)
+	for response in responses:
+		data = response.json()
+		all_orders.extend(data)
+	print('. Got {:,d} orders.'.format(len(all_orders)))
+	return all_orders
+
+def get_item_prices(response):
+	item_prices = {}
+	for index in range(0, len(response)):
+		type_id = response[index]['type_id']
+	
+		if not str(type_id) in item_prices:
+			item_prices[str(type_id)] = {}
+	
+		#add price info
+		if response[index]['is_buy_order'] == True:
+			if 'buy_price' in item_prices[str(type_id)]:
+				item_prices[str(type_id)]['buy_price'] = max(response[index]['price'], item_prices[str(type_id)]['buy_price'])
+			else:
+				item_prices[str(type_id)]['buy_price'] = response[index]['price']
+		else:
+			if 'sell_price' in item_prices[str(type_id)]:
+				item_prices[str(type_id)]['sell_price'] = min(response[index]['price'], item_prices[str(type_id)]['sell_price'])
+			else:
+				item_prices[str(type_id)]['sell_price'] = response[index]['price']
+	return item_prices
+
+def evaluate_contract(contract):
+	
+
+	if contract["type"] != 'item_exchange':
+		return {'profit_sell': 0, 'profit_buy':0}
+
+	contract_id = contract['contract_id']
+	cost = contract['price'] - contract['reward']
+	
+	#items 
+	# /v1/contracts/public/items/{par}/
+	response = esi_calling.call_esi(scope = '/v1/contracts/public/items/{par}/', url_parameter=contract_id, job = 'get region contract items')
+	raw_items = response.json()
+	
+	value_sell = 0
+	value_buy = 0
+	
+	for item_dict in raw_items:
+		if 'is_blueprint_copy' in item_dict:
+			continue
+		quantity = item_dict['quantity']
+		type_id = item_dict['type_id']
+		
+		if str(type_id) in item_prices:
+			if 'sell_price' in item_prices[str(type_id)]:
+				value_sell = value_sell + quantity * item_prices[str(type_id)]['sell_price']
+			if 'buy_price' in item_prices[str(type_id)]:
+				value_buy = value_buy + quantity * item_prices[str(type_id)]['buy_price']
+	
+	
+	profit = {'profit_sell': value_sell - cost, 'profit_buy':value_buy - cost}
+	return profit
+
+
+
+def import_prices():
+	#Import Jita prices and save
+	orders = import_orders(10000002)
+	item_prices = get_item_prices(orders)
+	with open('item_prices.json', 'w') as outfile:
+		json.dump(item_prices, outfile, indent=4)
+
+def analyze_contracts():
+	all_contracts = fetch_contracts(10000002)
+		
+	contract_values = {}
+	profitables = {'profit_sell':{}, 'profit_buy':{}}
+	number_of_contracts = len(all_contracts)
+	index = 1
+	for contract in all_contracts:
+		print('impoting ', index, '/', number_of_contracts)
+		index = index + 1
+		
+		profit = evaluate_contract( contract)
+		
+		contract_values[contract['contract_id']] = {'profit_sell':profit['profit_sell'], 'profit_buy':profit['profit_buy']}
+		with open('contract_values.json', 'w') as outfile:
+				json.dump(contract_values, outfile, indent=4)
+		
+		if profit['profit_buy'] > 0:
+			#profitable contract
+			clickable = "<url=contract:30003576//" + str(contract['contract_id']) + ">Profit sell</url>" + str(round(profit['profit_buy']) )
+			profitables['profit_buy'][contract['contract_id']] = clickable
+			with open('profit.txt', 'w') as outfile:
+				json.dump(profitables, outfile, indent=4)
+				
+		elif profit['profit_sell'] > 0:
+			#profitable contract
+			clickable = "<url=contract:30003576//" + str(contract['contract_id']) + ">Profit sell</url>" + str(round(profit['profit_sell']) )
+			profitables['profit_sell'][contract['contract_id']] = clickable
+			with open('profit.txt', 'w') as outfile:
+				json.dump(profitables, outfile, indent=4)
+	print('Analysis completed')
+
+def import_regions():
+	response = esi_calling.call_esi(scope = '/v1/universe/regions/', job = 'get regions')
+	
+	regions = {}
+	for region_id in response.json():
+		print('importing a region name...')
+		response = esi_calling.call_esi(scope = '/v1/universe/regions/{par}/', url_parameter=region_id, job = 'get region name')
+		region_name = response.json()["name"]
+		regions[region_name] = region_id
+		
+	with open('regions.json', 'w') as outfile:
+		json.dump(regions, outfile, indent=4)
+	
+def region_selection():
+	print('Regions: ', regions.keys() )
+	user_input = input("Type in the region to import ")
+	
+	if user_input in regions:
+		config['region'] = user_input
+		main_menu()
+	else:
+		print('Invalid region. Try again')
+		region_selection()
+	
+
+				
+def main_menu():
+	print('[R] Region to import (currently: ', config['region'], ')\n[M] Market data reimport\n[S] Start contract analysis')
+	user_input = input("[R/M/S] ")
+	if user_input in ['R', 'r']:
+		region_selection()
+	elif user_input in ['M', 'm']:
+		import_prices()
+		main_menu()
+	elif user_input in ['S', 's']:
+		analyze_contracts()
+		main_menu()
+	else:
+		main_menu()
+#--------------------------
+#Start the thing
+#--------------------------
+print('starting')
+
+try:
+	item_prices = json.load(open('item_prices.json'))
+except:
+	import_prices()
+
+try:
+	regions = json.load(open('regions.json'))
+except:
+	import_regions()
+
+try:
+	config = json.load(open('config.json'))
+except:
+	config = {'region':'Jita'}
+	with open('config.json', 'w') as outfile:
+		json.dump(config, outfile, indent=4)
+	
+main_menu()
+
+
+
+print('done')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ esi_calling.py
@@ -0,0 +1,216 @@
+import requests
+import json
+import time
+import base64
+import random
+import sys
+import webbrowser
+
+from datetime import datetime
+from datetime import timedelta
+
+def error_handling(esi_response, number_of_attempts, tokens = None, scope = None, job = None):
+	#Call this function to decide what to do on error
+	#Some arbitrary maximum try ammount
+	if number_of_attempts == 50:
+		print('There has been 50 failed attemts to call ESI. Something may be wrong.')
+		input('Press enter to continue trying...')
+		number_of_attempts = 1
+	
+	if job != '':
+		job_description = 'Failed to ' + job
+	
+	print(datetime.utcnow().strftime('%H:%M:%S'), job_description+'. Error',esi_response.status_code, end="")
+	
+	#Some errors have no description so try to print it
+	try:
+		print(' -', esi_response.json()['error'])
+	except:
+		#No error description from ESI
+		print('')
+		
+	if esi_response.status_code == 420:
+		#error limit reached. Wait until reset and try again.
+		time.sleep(esi_response.headers['x-esi-error-limit-reset']+1)
+	elif esi_response.status_code in [400, 401, 403]:
+		#Authorization not working
+		#Check if access token expired (can be fixed)
+		#Check if scopes are valid (can't be fixed)
+		#If can't be fixed then exit
+		print('Your authorization is the problem. Make sure your client ID and secret key are valid and that you logged the right char.')
+		input('Press enter to continue trying (won\'t work. Just close the script and redo login or client ID/secret)...')
+	
+	else:
+		#500 = internal server error (downtime?)
+		#502 = bad gateway
+		#503 = service unavailable
+		#Other errors
+		#Lets just wait a sec and try again and hope for best
+		time_to_wait = (2 ** number_of_attempts) + (random.randint(0, 1000) / 1000)
+		print('Retrying in', time_to_wait, 'second...')
+		time.sleep((2 ** number_of_attempts) + (random.randint(0, 1000) / 1000))
+
+
+def logging_in(scopes, client_id, client_secret):
+	number_of_attempts = 1
+	login_url = 'https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=http://localhost/oauth-callback&client_id='+client_id+'&scope='+scopes
+
+	webbrowser.open(login_url, new=0, autoraise=True)
+
+	authentication_code = input("Give your authentication code: ")
+	
+	combo = base64.b64encode(bytes( client_id+':'+client_secret, 'utf-8')).decode("utf-8")
+	authentication_url = "https://login.eveonline.com/oauth/token"
+	
+	esi_response = requests.post(authentication_url, headers =  {"Authorization":"Basic "+combo}, data = {"grant_type": "authorization_code", "code": authentication_code} )
+	
+	if esi_response.status_code != 200:
+		error_handling(esi_response, number_of_attempts, job = 'log in')
+	else:
+		tokens = {}
+		tokens['refresh_token'] = esi_response.json()['refresh_token']
+		tokens['access_token'] = esi_response.json()['access_token']
+		tokens['expiry_time'] = str( datetime.utcnow() + timedelta(0,esi_response.json()['expires_in']) )
+
+	
+	return tokens
+	
+	
+def check_tokens(tokens, client_secret, client_id):
+	#Check if access token still good
+	#If access token too old or doesn't exist generate new access token
+	
+	#refresh_token = tokens['refresh_token']
+	#access_token = tokens['access_token'] (optional)
+	#expiry_time = tokens['expiry_time'] (optional. Should exist with access token)
+	
+	number_of_attempts = 1
+	
+	
+	#Check if token is valid
+	#Needs to be done like this since the expiry time may or may not exist
+	if 'expiry_time' in tokens:
+		if datetime.utcnow() < datetime.strptime(tokens['expiry_time'], '%Y-%m-%d %H:%M:%S.%f'):
+			valid = True
+		else:
+			valid = False
+	else:
+		valid = False
+		
+			
+	
+	if not valid:
+		#No "expiry time" or the token has expired already
+		#No valid access token. Make new.
+		refresh_url = 'https://login.eveonline.com/oauth/token'
+		combo = base64.b64encode(bytes( client_id+':'+client_secret, 'utf-8')).decode("utf-8")
+		
+		trying = True
+		while trying == True:
+			esi_response = requests.post(refresh_url, headers =  {"Authorization":"Basic "+combo}, data = {"grant_type": "refresh_token", "refresh_token": tokens['refresh_token']} )
+			
+			if esi_response.status_code != 200:
+				error_handling(esi_response, number_of_attempts, tokens, scope = None, job = 'refresh tokens')
+			else:
+				#Success
+				trying = False
+			number_of_attempts = number_of_attempts + 1
+			
+		tokens['refresh_token']	= esi_response.json()['refresh_token']
+		tokens['access_token'] = esi_response.json()['access_token']
+		tokens['expiry_time'] = str( datetime.utcnow() + timedelta(0,esi_response.json()['expires_in']) )
+		
+	return tokens
+		
+
+def get_token_info(tokens):
+	#Uses the access token to get various info
+	#character ID
+	#character name
+	#expiration time (not sure on format)
+	#scopes
+	#token type (char/corp)
+	
+	url = 'https://login.eveonline.com/oauth/verify'
+	
+	trying = True
+	while trying == True:
+		esi_response = requests.get(url, headers =  {"Authorization":"Bearer "+tokens['access_token']})
+		
+		if esi_response.status_code != 200:
+			error_handling(esi_response, number_of_attempts, tokens, scope = None, job = 'get token info')
+		else:
+			#Success
+			trying = False
+			
+	token_info = {}
+	token_info['character_name'] = esi_response.json()['CharacterName']
+	token_info['character_id'] = esi_response.json()['CharacterID']	
+	token_info['expiration'] = esi_response.json()['ExpiresOn']	
+	token_info['scopes'] = esi_response.json()['Scopes']	
+	token_info['token_type'] = esi_response.json()['TokenType']	
+	
+	return token_info
+		
+def call_esi(scope, url_parameter = '', parameters={}, etag = None, tokens = None, datasource = 'tranquility', calltype='get', job = ''):
+	#scope = url part. Mark the spot of parameter with {par}
+	#url_parameter = parameter that goes into the url
+	#parameters = json parameters to include (pages mostly)
+	#etag = TODO
+	#tokens = json that contains refresh token. Optinally also access token and its expiration time if they already exist.
+	#refresh_token = tokens['refresh_token']
+	#access_token = tokens['access_token'] (optional)
+	#expiry_time = tokens['expiry_time'] (optional. Should exist with access token)
+	
+	#datasource. Default TQ
+	#calltype = get, post or delete. Default get
+	#job = string telling what is being done. Is displayed on error message.
+	
+	number_of_attempts = 0
+		
+	
+	#Build the url to call to
+	#Also replace // with / to make things easier
+	url = 'https://' + ('esi.evetech.net'+scope+'/?datasource='+datasource).replace('{par}', str(url_parameter)).replace('//', '/')
+
+	
+	#print(url)
+	
+	#un-authorized / authorized
+	if tokens == None:
+		headers = {}
+	else:
+		headers =  {"Authorization":"Bearer "+tokens['access_token']}
+	
+	trying = True
+	while trying == True:
+		#Make the call based on calltype
+		if calltype == 'get':
+			esi_response = requests.get(url, headers = headers, params = parameters)
+		elif calltype == 'post':
+			esi_response = requests.post(url, headers = headers, params = parameters)
+		elif calltype == 'delete':
+			esi_response = requests.post(url, headers = headers, params = parameters)
+		
+		#200 = ok
+		#204 = ok
+		#304 = no change
+		#404 = not found
+		#400 = bad request. User is stupid
+		if esi_response.status_code in [200, 204, 304]:
+			#All OK
+			trying = False
+		elif esi_response.status_code in [404,  400]:
+			#print('404 not found')
+			trying = False
+		else:
+			error_handling(esi_response, number_of_attempts, tokens, scope, job)
+
+		number_of_attempts = number_of_attempts + 1
+		
+	return esi_response
+	
+	
+	
+	
+	
